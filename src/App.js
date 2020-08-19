519fce3cf40cff78d1691a2190dfcd17226ee713
MIlan <milan@pop-os.localdomain>
2020-08-19 18:32:39 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homePage.component';
+import { Route, Switch } from 'react-router-dom';
+
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <Homepage/>
+      <Switch>
+        <Route exact path="/" component={Homepage}/>
+        <Route exact path="/hats" component={HatsPage}/>
+      </Switch>
     </div>
   );
 }
