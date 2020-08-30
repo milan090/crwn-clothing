d2e7f99e00a8c93a297c61281e2d84ec840be6f5
MIlan <milan@pop-os.localdomain>
2020-08-30 11:59:18 +0530

    Made header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homePage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 import { Route, Switch } from 'react-router-dom';
 
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path="/" component={Homepage}/>
         <Route exact path="/shop" component={ShopPage} />
