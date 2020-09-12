aaffa3a2579e18114bd159d514de102bcc5bb992
milan090 <milanmuhammed1@gmail.com>
2020-09-12 14:51:29 +0530

    added sign in with email and password with firebase

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -4,7 +4,7 @@ import CustomButton from '../custom-button/custom-buttton.component';
 
 import './sign-in.styles.scss';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 
 class SignIn extends React.Component {
@@ -17,9 +17,17 @@ class SignIn extends React.Component {
     }
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
-    this.setState({email: '', password: ''});
+    
+    const { email, password } = this.state;
+    
+    try {
+      const { user } = await auth.signInWithEmailAndPassword(email, password);
+      this.setState({email: '', password: ''});
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   handleChange = event => {
